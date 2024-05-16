d1e7d3f7d1518c4086bf08dfa8ff2317161214ad
naopeke <naopeke@protonmail.com>
2024-05-16 13:22:57 +0200

    basic settings

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const  pool = new Pool ({
-    user: process.env.DB_USER,
+    user: process.env.DB_USERNAME,
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
