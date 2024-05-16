2ca492f175f40b95b879bea494b6cc9b87ea0212
naopeke <naopeke@protonmail.com>
2024-05-16 15:12:34 +0200

    basic settings

--- src/app.js
+++ src/app.js
@@ -9,12 +9,8 @@ app.set('port', process.env.PORT || 3000);
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(generalRouters); // routers.jsで定義されたルーターを使用する
-
-// 404 Not Foundのエラーハンドリングを追加
+app.use(generalRouters);
 app.use(handle404);
-
-// エラーハンドリングミドルウェアを最後に追加
 app.use(errorHandling);
 
 module.exports = app;
