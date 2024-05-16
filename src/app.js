e80b3117f3963d56c21557e3b4dbbe809f50481a
naopeke <naopeke@protonmail.com>
2024-05-16 15:05:31 +0200

    basic settings

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const generalRouters = require('./routers/routers');
-const errorHandling = require('./error/errorHandling');
+const { errorHandling, handle404 } = require('./error/errorHandling');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -9,10 +9,12 @@ app.set('port', process.env.PORT || 3000);
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(generalRouters);
-app.use(function (req, res, next){
-    res.status(404).json({ error:true, code:404, message: 'Endpoint is not found'})
-});
+app.use(generalRouters); // routers.jsで定義されたルーターを使用する
+
+// 404 Not Foundのエラーハンドリングを追加
+app.use(handle404);
+
+// エラーハンドリングミドルウェアを最後に追加
 app.use(errorHandling);
 
 module.exports = app;
