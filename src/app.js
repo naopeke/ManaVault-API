22e9f76d4d1f1a4d10ff568fb6326eacf896fe7d
naopeke <naopeke@protonmail.com>
2024-05-16 15:17:12 +0200

    basic settings

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const generalRouters = require('./routers/routers');
+const routers = require('./routers/routers');
 const { errorHandling, handle404 } = require('./error/errorHandling');
 const app = express();
 
@@ -9,7 +9,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(generalRouters);
+app.use(routers);
 app.use(handle404);
 app.use(errorHandling);
 
