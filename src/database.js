071cf3851fd3d4e7b51d6070eda03275c394affe
naopeke <naopeke@protonmail.com>
2024-05-16 13:42:40 +0200

    basic settings

--- src/database.js
+++ src/database.js
@@ -7,20 +7,20 @@ const  pool = new Pool ({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT
-});
+}).promise;
 
-(
-    async() => {
-        try {
-            await pool.connect();
-            const res = await pool.query('SELECT NOW()');
-            console.log(res.rows[0]);
-        } catch (err) {
-            console.error('Error occured during the connection to the DB', err);
-        } finally {
-            await pool.end();
-        }
-    }
-)();
+// (
+//     async() => {
+//         try {
+//             await pool.connect();
+//             const res = await pool.query('SELECT NOW()');
+//             console.log(res.rows[0]);
+//         } catch (err) {
+//             console.error('Error occured during the connection to the DB', err);
+//         } finally {
+//             await pool.end();
+//         }
+//     }
+// )();
 
 module.exports = { pool };
