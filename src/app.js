8ebdb316761308d88d0ef5b6b2f23a859585aae5
naopeke <naopeke@protonmail.com>
2024-05-24 14:10:54 +0200

    firebase

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 const routers = require('./routers/routers');
 const { errorHandling, handle404 } = require('./error/errorHandling');
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.use(cors());
+app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(routers);
--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,7 +1,10 @@
 const { Router } = require ('express');
 const router = Router();
+const { db } = require ('../firebase');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const querySnapshot = await db.collection('users').get();
+    console.log(querySnapshot.docs);
     res.send('API DESPLEGADA');
 });
 
