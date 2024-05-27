82bfc2e97606d598f3d4a591f658a95fa497fdc8
naopeke <naopeke@protonmail.com>
2024-05-27 13:26:27 +0200

    remove firebase.json

--- src/controller/initial.controller.js
+++ src/controller/initial.controller.js
@@ -2,7 +2,8 @@ const { db } = require('../firebase');
 
 const getCollection = async (req, res) => {
     const result = await db.collection('users').get();
-    console.log(result.docs[0].data());
+    console.log(result);
+    // console.log(result.docs[0].data());
     res.send('API DESPLEGADA');
 }
 
