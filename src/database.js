7feeb81e73168058c607a6745f00c8f9e3395749
naopeke <naopeke@protonmail.com>
2024-07-11 13:03:41 +0200

    database.js

--- src/database.js
+++ src/database.js
@@ -6,7 +6,10 @@ const  pool = new Pool ({
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    dialectModule: pg,
+    timezone: process.env.TZ
 });
 
 pool.on('connect', () => {
