f7e1f6ad70732196306a8b0cbfca3463ff32999f
naopeke <naopeke@protonmail.com>
2024-07-11 12:31:52 +0200

    changed routers.js

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,8 +1,12 @@
 const { Router } = require ('express');
 const router = Router();
 const ctrl = require('../controller/initial.controller');
+const userCtrl = require('../controller/user.controller');
+
 
 
 router.get('/', ctrl.getCollection);
+router.get('/:user_id', userCtrl.getUser);
+
 
 module.exports = router;
