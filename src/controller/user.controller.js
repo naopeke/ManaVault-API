7df31dd569de0bb86400d4c0cc20ca0ea54579ce
naopeke <naopeke@protonmail.com>
2024-07-12 11:29:15 +0200

    updateUser

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,37 +1,4 @@
 
-//   const loginUser = async(req, res) =>{
-//     const { email, password } = req.body;    
-
-//     try {
-//         const sql = 'SELECT * FROM manavault.user WHERE email = $1';
-//         const values = [email];
-//         const result = await pool.query(sql, values);
-
-//         if(result.rows.length > 0) {
-//            const user = result.rows[0];
-
-//            const userInstance = new User(user.userId, user.email, user.password, user.username, user.photoURL);
-
-//            //password
-//           //  const isValidPassword = await userInstance.verifyPassword(password);
-//           //  if (!isValidPassword){
-//           //   return res.status(401).send({message: 'Authentication failed'});
-//           //  }
-
-//            const userRecord = {
-//             userId: userInstance.userId,
-//             username: userInstance.username,
-//             email: userInstance.email
-//            }
-//           res.status(200).send({ userRecord, message: 'User authenticated successfully' });
-//         } else {
-//           res.status(401).send({ message: 'Authentication failed' });
-//         }
-//       } catch (err) {
-//         console.error('Error:', err);
-//         res.status(401).send({ message: 'Authentication failed' });
-//       }
-//   }
 
 //   const updateUser = async (req, res) => {
 //     const { userId } = req.params;
@@ -70,17 +37,6 @@
 //       res.status(400).send(error);
 //     }
 //   };
-
-
-
-// module.exports = {
-//     registerUser,
-//     getUser,
-//     loginUser,
-//     updateUser,
-//     deleteUser
-// }
-
 
 
 
@@ -124,12 +80,7 @@ const registerUser = async (req, res) => {
   const { username, email, password } = req.body;
 
   try {
-
-    // //hash password
-    // const saltRounds = 10;
     const user = new User(null, email, password, username, null);
-    // await user.hashPassword(saltRounds);
-
     const sql = 'INSERT INTO manavault.users (username, email, password) VALUES ($1, $2, $3) RETURNING user_id';
     const values = [user.username, user.email, user.password];
     const result = await client.query(sql, values);
@@ -145,7 +96,7 @@ const registerUser = async (req, res) => {
 };
 
 
-
+//login
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
@@ -157,7 +108,7 @@ const loginUser = async (req, res) => {
     if (result.rows.length > 0) {
       const user = result.rows[0];
 
-      // パスワードの比較
+      //compare password
       if (password === user.password) {
         const userInstance = new User(user.userId, user.email, user.password, user.username, user.photoURL);
 
@@ -169,11 +120,11 @@ const loginUser = async (req, res) => {
 
         res.status(200).send({ userRecord, message: 'User authenticated successfully' });
       } else {
-        // パスワードが一致しない場合
+        //when the password doesn't match
         res.status(401).send({ message: 'Authentication failed' });
       }
     } else {
-      // ユーザーが見つからない場合
+      //cannot find the user
       res.status(401).send({ message: 'Authentication failed' });
     }
   } catch (err) {
@@ -183,8 +134,72 @@ const loginUser = async (req, res) => {
 }
 
 
+//edit userRecord
+const updateUser = async (req, res) => {
+  const { user_id } = req.params;
+  const { email, username, img_uri } = req.body;
+
+  // try {
+  //   const sql = 'UPDATE manavault.users SET email = $1, username = $2, img_uri = $3 WHERE user_id = $4 RETURNING *';
+  //   const values = [email, username, img_uri, user_id];
+  //   const result = await client.query(sql, values);
+
+  //   if (result.rows.length > 0){
+  //     const userRecord = result.rows[0];
+  //     console.log('Successfully updated user', userRecord);
+  //     res.status(200).send(userRecord);
+  //   }
+
+  // } catch (error) {
+  //   console.log('Error updating user:', error);
+  //   res.status(400).send(error);
+  // }
+
+  try {
+  let sql = 'UPDATE manavault.users SET';
+  let values = [];
+  let index = 1;
+
+  if (email){
+    sql += ` email = $${index},`;
+    values.push(email);
+    index++;
+  }
+
+  if (username){
+    sql += ` username = $${index},`;
+    values.push(username);
+    index++;
+  }
+
+  if (img_uri){
+    sql += ` img_uri = $${index},`;
+    values.push(img_uri);
+    index++;
+  }
+
+  sql = sql.slice(0, -1); //delete the last ',' and space
+  sql += ` WHERE user_id = $${index} RETURNING *`;
+  values.push(user_id);
+
+  const result = await client.query(sql, values);
+
+  if (result.rows.length > 0){
+    const userRecord = result.rows[0];
+    console.log('Successfully updated user', userRecord);
+    res.status(200).send(userRecord);
+  }
+
+} catch (error) {
+  console.log('Error updating user:', error);
+  res.status(400).send(error);
+}
+};
+
+
   module.exports = {
     getUser,
     registerUser,
     loginUser,
+    updateUser,
   }
--- src/routers/general.routers.js
+++ src/routers/general.routers.js
@@ -5,7 +5,7 @@ const userCtrl = require('../controller/user.controller');
 router.post('/register', userCtrl.registerUser);
 router.get('/:user_id', userCtrl.getUser);
 router.put('/login', userCtrl.loginUser);
-// router.post('/users/:user_id', userCtrl.updateUser);
+router.post('/users/:user_id', userCtrl.updateUser);
 // router.delete('/users/:user_id', userCtrl.deleteUser);
 
 
